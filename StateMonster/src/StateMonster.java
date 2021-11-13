cf127e7212ebb7583f0cc87ad8bfd91dd7a035c6
s20184 <s20184@pjwstk.edu.pl>
2021-11-13 22:38:16 +0100

    Added state skeleton for the Monster

--- StateMonster/src/StateMonster.java
+++ StateMonster/src/StateMonster.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class StateMonster {
+public class StateMonster {
+    // Example of handling different behaviors depending on state,
+    // using the state pattern, including the transitions between states
+
+
+}
+
+abstract class State {
+
+}
+
+class Idle extends State {
+
+}
+
+class Active extends State {
+
+}
+
+class Aggressive extends State {
+
+}
+
+class Dead extends State {
+
 }
