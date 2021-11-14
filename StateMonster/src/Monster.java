f2846b6899c8e2337054aa8b922b2a987c917be0
s20184 <s20184@pjwstk.edu.pl>
2021-11-14 21:08:46 +0100

    Added Monster class and described all States

--- StateMonster/src/Monster.java
+++ StateMonster/src/Monster.java
@@ -1,4 +1,35 @@
 public class Monster {
-    boolean searching;
+    private State state;
+    private int playersInRange = 0;
+    boolean alive = true;
 
+    public Monster(){
+        this.state = new IdleState(this);
+    }
+
+    public void changeState(State state){
+        this.state = state;
+    }
+
+    public State getState(){
+        return this.state;
+    }
+
+    public void addPlayer(){
+        this.playersInRange++;
+    }
+
+    public void removePlayer(){
+        this.playersInRange--;
+    }
+
+    public void kill() { this.alive = false; }
+
+    public void revive() { this.alive = true; }
+
+    public boolean isAlive() { return this.alive; }
+
+    public boolean hasPlayersInRange(){
+        return playersInRange == 0;
+    }
 }
--- StateMonster/src/State.java
+++ StateMonster/src/State.java
@@ -1,15 +1,41 @@
-abstract class State {
-    private Monster monster;
+public abstract class State {
+    Monster monster;
 
     State (Monster monster){
         this.monster = monster;
     }
+
+    public abstract String playerEnteredSight();
+    public abstract String playerLeftSight();
+    public abstract String monsterKilled();
+    public abstract String monsterRevived();
 }
 
 class IdleState extends State {
 
     IdleState(Monster monster){
         super(monster);
+    }
+
+    @Override
+    public String playerEnteredSight() {
+        return "Monster can't attack when idle!";
+    }
+
+    @Override
+    public String playerLeftSight(){
+        return "Monster likely never saw a player to begin with.";
+    }
+
+    @Override
+    public String monsterKilled() {
+        monster.changeState(new DeadState(monster));
+        return "Monster now dead (due to natural causes).";
+    }
+
+    @Override
+    public String monsterRevived() {
+        return null;
     }
 }
 
@@ -18,6 +44,28 @@ class ActiveState extends State {
     ActiveState(Monster monster){
         super(monster);
     }
+
+    @Override
+    public String playerEnteredSight() {
+        monster.changeState(new AggressiveState(monster));
+        return "Monster now attacking!";
+    }
+
+    @Override
+    public String playerLeftSight() {
+        return "Monster likely never saw a player to begin with";
+    }
+
+    @Override
+    public String monsterKilled() {
+        monster.changeState(new DeadState(monster));
+        return "Monster now dead (due to natural causes).";
+    }
+
+    @Override
+    public String monsterRevived() {
+        return null;
+    }
 }
 
 class AggressiveState extends State {
@@ -25,11 +73,70 @@ class AggressiveState extends State {
     AggressiveState(Monster monster){
         super(monster);
     }
+
+    @Override
+    public String playerEnteredSight() {
+        monster.addPlayer();
+        return "Monster is hunting more players!";
+    }
+
+    @Override
+    public String playerLeftSight(){
+        monster.removePlayer();
+        if(monster.hasPlayersInRange()) {
+            return "Monster lost sight of one player, but more remain!";
+        } else {
+            monster.changeState(new IdleState(monster));
+            return "Monster has lost sight of all players and is now idle.";
+        }
+    }
+
+    @Override
+    public String monsterKilled() {
+        monster.kill();
+        monster.changeState(new DeadState(monster));
+        return "Monster has died.";
+    }
+
+    @Override
+    public String monsterRevived() {
+        return "Monster can't be revived when alive!";
+    }
 }
 
 class DeadState extends State {
 
     DeadState(Monster monster){
         super(monster);
+    }
+
+    @Override
+    public String playerEnteredSight() {
+        monster.addPlayer();
+        return "Monster can't do anything because it's dead...";
+    }
+
+    @Override
+    public String playerLeftSight(){
+        monster.removePlayer();
+        return "Monster can't do anything because it's dead...";
+    }
+
+    @Override
+    public String monsterKilled() {
+        monster.kill();
+        return "Monster now killed more than once?";
+    }
+
+    @Override
+    public String monsterRevived() {
+        monster.revive();
+        if(monster.hasPlayersInRange()) {
+            this.monster.changeState(new AggressiveState(monster));
+            return "Monster has been revived and is hunting!";
+        } else {
+            this.monster.changeState(new IdleState(monster));
+            return "Monster has been revived and is idle.";
+        }
     }
 }
